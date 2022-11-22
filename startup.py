c19e94f57cb05b1a03339cf646357eeefecf2abd
Aaron Ripley <ripleya1@xavier.edu>
2022-11-21 22:02:39 -0500

    .

--- startup.py
+++ startup.py
@@ -3,24 +3,22 @@ import platform
 import subprocess
 
 def startServer():
+    makeMigrationsProc = subprocess.Popen(["python", "manage.py", "makemigrations"], shell=True)
+    makeMigrationsProc.wait()
     migrateProc = subprocess.Popen(["python", "manage.py", "migrate"], shell=True)
     migrateProc.wait()
-    # global runserverProcess 
     runserverProcess = subprocess.Popen(["python", "manage.py" ,"runserver"], shell=True)
-    # yield(runserverProcess)
-    yield
-    runserverProcess.terminate()
+    return(runserverProcess)
 
-    try:
-        if(platform.system() == "Windows"):
-            system("del databases/db.sqlite3")
-        else:
-            system("rm databases/db.sqlite3")
-    except Exception as e:
-        print("Exception: " + e)
+def stopServer(runServerProcess):
+    runServerProcess.terminate()
+    # try:
+    #     if(platform.system() == "Windows"):
+    #             system("del databases/db.sqlite3")
+    #     else:
+    #             system("rm databases/db.sqlite3")
+    # except Exception as e:
+    #     print("Exception: " + e)
 
-    system("python manage.py makemigrations")
-    system("python manage.py migrate")
-    system("python manage.py runserver")
-
-startServer()
+proc = startServer()
+stopServer(proc)
